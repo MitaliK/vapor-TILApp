d72e2fbeb5b702d827cde3ab4cf3bcf826d0484c
Mitali Kulkarni <kulkarnimitali20@gmail.com>
2018-09-06 10:46:41 -0700

    use bootstrap to style pages and add navigation bar

--- Sources/App/Controllers/WebsiteController.swift
+++ Sources/App/Controllers/WebsiteController.swift
@@ -14,6 +14,12 @@ struct WebsiteController: RouteCollection {
         
         // This registers the acronymHandler route for /acronyms/<ACRONYM ID>
         router.get("acronyms", Acronym.parameter, use: acronymHandler)
+        
+        // This registers the route for /users/<USER ID>, like the API
+        router.get("users", User.parameter, use: userHandler)
+        
+        // This registers the route for /users/
+        router.get("users", use: allUsersHandler)
     }
     
     // 4. Implement indexHandler(_:) that returns Future<View>
@@ -48,6 +54,35 @@ struct WebsiteController: RouteCollection {
             })
         })
     }
+    
+    // 1 Define the route handler for the user page that returns Future<View>.
+    func userHandler(_ req: Request) throws -> Future<View> {
+        
+        // 2 Get the user from the request’s parameters and unwrap the future.
+        return try req.parameters.next(User.self).flatMap(to: View.self) { user in
+            
+            // 3 Get the user’s acronyms using the computed property and unwrap the future.
+            return try user.acronyms.query(on: req).all().flatMap(to: View.self) { acronyms in
+                
+                // 4 Create a UserContext, then render the user.leaf template, returning the result. In this case, you’re not setting the acronyms array to nil if it’s empty. This is not required as you’re checking the count in template
+                    let context = UserContext(title: user.name,user: user, acronyms: acronyms)
+                    return try req.view().render("user", context)
+            }
+        }
+    }
+    
+    // 1. Define a route handler for the “All Users” page that returns Future<View>.
+    func allUsersHandler(_ req: Request) throws -> Future<View> {
+        
+        // 2. Get the users from the database and unwrap the future
+        return User.query(on: req).all().flatMap(to: View.self, { users in
+            
+            // 3. Create an AllUsersContext and render the allUsers.leaf template, then return the result.
+            let context = AllUsersContext(title: "All Users", users: users)
+
+            return try req.view().render("allUsers", context)
+        })
+    }
 }
 
 // Leaf uses Codable to handle Data
@@ -62,4 +97,17 @@ struct AcronymContext: Encodable {
     let title: String
     let acronym: Acronym
     let user: User
+}
+
+// This context contains inform about acronym and its user
+struct UserContext: Encodable {
+    let title: String
+    let user: User
+    let acronyms: [Acronym]
+}
+
+// This context contains a title and an array of users.
+struct AllUsersContext: Encodable {
+    let title: String
+    let users: [User]
 }
